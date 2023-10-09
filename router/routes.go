f887a2d3964cd640d24ab869992f36bf7166bcc9
Thalison Silva <contato.thalison@outlook.com>
2023-10-09 20:20:02 -0300

    fast: add-routes

--- router/routes.go
+++ router/routes.go
@@ -13,5 +13,29 @@ func initializeRoutes(router *gin.Engine) {
 				"msg": "GET Opening",
 			})
 		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
+			})
+		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
+			})
+		})
+
+		v1.GET("/openings", func(ctx *gin.Context) { // Modificado para "/openings"
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Openings",
+			})
+		})
 	}
 }
